5a1c59a540115ae337649369f89531d450147ac9
Marii R <mariia.revak@gmail.com>
2025-02-17 20:25:10 +0200

    edits

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const menu = document.querySelector('.mobile-menu'); 
+const openBtn = document.getElementById('burger-btn'); 
+const closeBtn = document.getElementById('mobile-menu-close'); 
+
+// Функція для відкриття меню
+openBtn.addEventListener('click', () => { 
+  menu.classList.add('is-open'); 
+  document.body.style.overflow = 'hidden'; // Забороняємо скрол під час відкритого меню
+}); 
+
+// Функція для закриття меню
+closeBtn.addEventListener('click', () => { 
+  menu.classList.remove('is-open'); 
+  document.body.style.overflow = ''; // Відновлюємо скрол
+}); 
+
+// Закриття при кліку поза меню
+document.addEventListener('click', (e) => { 
+  if (!menu.contains(e.target) && !openBtn.contains(e.target)) { 
+    menu.classList.remove('is-open'); 
+    document.body.style.overflow = ''; 
+  } 
+});
+
+const modal = document.getElementById('backdrop');
+const openModalBtn = document.querySelector('.hero-btn');
+const closeModalBtn = document.getElementById('close-modal');
+
+openModalBtn.addEventListener('click', () => {
+    modal.classList.add('is-open');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+});
+
+// Закриття по кліку за межами модального вікна
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+      modal.classList.remove('is-open');
+  }
+});
+
+
